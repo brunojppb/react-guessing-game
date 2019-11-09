b0efd6744fe40ea6b97eb907c22408a7652f9ae7
Bruno Paulino <brunojppb@gmail.com>
2019-11-09 14:51:42 +0100

    guessing game complete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,96 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
+function generateSecretNumber() {
+  return Math.round(Math.random() * 100);
+}
+
 function App() {
+
+  const [secretNumber, setSecretNumber] = useState(generateSecretNumber());
+  const [currentGuess, setCurrentGuess] = useState('');
+  const [guesses, setGuesses] = useState([]);
+  
+  console.log('the secret number is: ', secretNumber);
+  const didWin = guesses.includes(secretNumber.toString());
+
+  const onChangeGuess = (event) => {
+    const {value} = event.target;
+    const intValue = parseInt(value);
+    if (intValue) {
+      setCurrentGuess(intValue.toString());
+    } else if (value === '') {
+      setCurrentGuess('');
+    }
+  };
+
+  const onTryGuess = () => {
+    if (currentGuess !== '' && !guesses.includes(currentGuess)) {
+      setGuesses([currentGuess, ...guesses]);
+    }
+  };
+
+  const resetGame = () => {
+    window.location.reload();
+  }
+
   return (
     <div className="App-Container">
       <h1>React Guessing Game</h1>
       <p>
-        Tente adivinhar o número secreto.
+        try to guess the secret number between <strong>0</strong> and <strong>100</strong>
       </p>
-      <input name="guess" placeholder="Qual o número secreto?"/>
+      
+      {/* Container for input and button */}
+      <div className="input-container">
+        <input name="guess" 
+              placeholder="What is the number?" 
+              onChange={onChangeGuess}
+              value={currentGuess}/>
+        <button className="btn guess-btn" disabled={currentGuess === ''} onClick={onTryGuess}>Try!</button>
+      </div>
+
+      {/* Container for user guesses */}
+      <div className="guesses-container">
+        <ul>
+        { guesses.map(number => <Guess key={number} value={number} secretNumber={secretNumber}/>) }
+        </ul>
+      </div>
+      <div className="reset-controls">
+        <button className="btn" onClick={resetGame}>Reset Game</button>
+      </div>
+
+      { didWin ? <Winner resetCallback={resetGame}/> : null }
+
     </div>
   );
+}
+
+function Guess({value, secretNumber}) {
+
+  const isTooHigh = value > secretNumber;
+  const color = isTooHigh ? 'yellow' : 'white';
+  const text = isTooHigh ? 'Too high' : 'Too low';
+
+  return(
+    <li style={{color}}>
+      {value} - {text}
+    </li>
+  );
+
+}
+
+function Winner(props) {
+
+  return(
+    <div className="winner-container">
+      <div className="winner-message">
+        <p>Congratulations! 🎉</p>
+        <button className="btn" onClick={props.resetCallback}>Play Again</button>
+      </div>
+    </div>
+  )
+
 }
 
 export default App;
