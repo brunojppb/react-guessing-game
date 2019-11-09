63ad14a72b2a9681fc505b195f45f52f0a53f5c5
Bruno Paulino <brunojppb@gmail.com>
2019-11-09 12:10:02 +0100

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-Container">
+      <h1>React Guessing Game</h1>
+      <p>
+        Tente adivinhar o número secreto.
+      </p>
+      <input name="guess" placeholder="Qual o número secreto?"/>
     </div>
   );
 }
